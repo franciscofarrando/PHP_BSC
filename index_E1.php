23e220021b56ea477dc6c87fe1fb93d24aa1cd58
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2023-03-09 13:08:14 +0100

    13:08

--- index_E1.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $var = array(123,2.5,"hola",false);
-
-    echo "<pre>";
-    var_dump($var); 
-?>
--- index_E3.php
+++
@@ -1,4 +0,0 @@
-<?php
-define("nombre","Francisco Farrando");
-echo "<h1><b>". nombre ."</b></h1>";
-?>
